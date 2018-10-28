35554a6c9b6e680b8ce792909d61910c9919a1b5
jixinchan <1209980074@qq.com>
2018-10-28 22:28:56 +0800

    7

--- chap04-process/entropy.js
+++ chap04-process/entropy.js
@@ -2,42 +2,62 @@
 
 const err = console.error,
       log = console.log,
-      stdin = process.stdin,
-      sum=0,
-      hp=0,
-      arr=[];
+      stdin = process.stdin;
 
-var j=0;
-function errDeal(){
+var j=0,
+    arr = [],
+    sum = 0,
+    hp = 0;
+
+//主函数
+function main(){
   if(process.argv.length<3){
-    err('无命令行参数！');
+    loopInput();
+  }else{
+    for(var p=2;p<process.argv.length;p++){
+      arr.push(argv[i]);
+    }
+    errDeal();
+  }
+}
+
+//错误处理
+function errDeal(){
+  
+}
+function judgeOne(v){
+  if(v<0||v>1){
+    err('命令行参数错误：请输入0-1的数！');
+    return;
+  }else if(!Number(v)){
+    err('命令行参数错误：请输入数字！');
     return;
   }
-  for(var i=2;i<process.argv.length;i++){
-    if(argv[i]<0||argv[i]>1){
-      err('命令行参数错误：请输入0-1的数！');
-      return;
-    }else if(!Number(argv[i])){
-      err('命令行参数错误：请输入数字！');
-      return;
-    }
-    sum+=argv[i];
+}
+function judgeSum(){
+  for(var q = 0;q<arr.length;q++){
+    sum+=arr[q];
   }
   if(sum<1){err('命令行参数错误：概率空间不封闭！');}
   else if(sum>1){err('命令行参数错误：概率和超过1！');}
 }
-log('p'+j);
-stdin.on('data',(data)=>{
-  j++;
-  log('p'+j);
-  arr.push(data);
-});
 
-stdin.on('end',()=>{
-  for(var m =0;m<arr.length;m++){
-    num = Number(arr[m]);
-    hp += -num*Math.log2(num);
-  }
-  console.log(hp);
-});
+//逐行输入
+function  loopInput(){
+  log('p'+j);
+
+  stdin.on('data',(data)=>{
+    j++;
+    log('p'+j);
+    arr.push(data);
+  });
+
+  stdin.on('end',()=>{
+    for(var m =0;m<arr.length;m++){
+      num = Number(arr[m]);
+      hp += -num*Math.log2(num);
+    }
+    console.log(hp);
+  });
+}
 
