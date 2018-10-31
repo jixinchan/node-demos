446d75007c819ec871d5f4ee08bb5d5bec35760b
jixinchan <1209980074@qq.com>
2018-10-31 22:54:16 +0800

    1

--- chap04-process/entropy.js
+++ chap04-process/entropy.js
@@ -7,7 +7,8 @@ const err = console.error,
 var j=0,
     arr = [],
     sum = 0,
-    hp = 0;
+    hp = 0,
+    flag=true;
 
 //主函数
 function main(){
@@ -23,32 +24,40 @@ function main(){
 
 //错误处理
 function errDeal(){
-  
+  arr.forEach(judgeOne(v));
+  judgeSum();
+  if(!flag){
+    loopInput();
+  }
 }
 function judgeOne(v){
   if(v<0||v>1){
     err('命令行参数错误：请输入0-1的数！');
-    return;
+    return false;
   }else if(!Number(v)){
     err('命令行参数错误：请输入数字！');
-    return;
+    return false;
   }
+  return true;
 }
 function judgeSum(){
   for(var q = 0;q<arr.length;q++){
     sum+=arr[q];
   }
-  if(sum<1){err('命令行参数错误：概率空间不封闭！');}
-  else if(sum>1){err('命令行参数错误：概率和超过1！');}
+  if(sum<1){err('命令行参数错误：概率空间不封闭！');return false;}
+  else if(sum>1){err('命令行参数错误：概率和超过1！');return false;}
+  return true;
 }
 
 //逐行输入
 function  loopInput(){
+  log('请输入信源熵概率分布')
   log('p'+j);
 
   stdin.on('data',(data)=>{
     j++;
     log('p'+j);
+    judgeOne(data);
     arr.push(data);
   });
 
@@ -57,6 +66,7 @@ function  loopInput(){
       num = Number(arr[m]);
       hp += -num*Math.log2(num);
     }
+    log('p=',arr);
     console.log(hp);
   });
 }
