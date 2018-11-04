ca4670c8108363d2e24deced210e557208662049
jixinchan <1209980074@qq.com>
2018-11-04 14:05:53 +0800

    entropy

--- chap04-process/entropy.js
+++ chap04-process/entropy.js
@@ -8,65 +8,98 @@ var j=0,
     arr = [],
     sum = 0,
     hp = 0,
-    flag=true;
+    f1=true,
+    f2=true;
 
 //主函数
 function main(){
   if(process.argv.length<3){
     loopInput();
   }else{
-    for(var p=2;p<process.argv.length;p++){
-      arr.push(argv[i]);
+    var p;
+    for(p=2;p<process.argv.length;p++){
+      arr.push(process.argv[p]);
     }
-    errDeal();
+    if(arr.every(judgeOne)&&judgeSum()){
+      getResult();
+    }else{
+      loopInput();
+    }
   }
 }
 
 //错误处理
-function errDeal(){
-  arr.forEach(judgeOne(v));
-  judgeSum();
-  if(!flag){
-    loopInput();
-  }
-}
 function judgeOne(v){
   if(v<0||v>1){
     err('命令行参数错误：请输入0-1的数！');
     return false;
   }else if(!Number(v)){
-    err('命令行参数错误：请输入数字！');
-    return false;
+    if(v.indexOf(',')&&!v.indexOf(' ')){
+      //log('argv:',v);
+      judgeS(v);
+      return true;
+    }else if(v.indexOf(',')&&v.indexOf(' ')){
+      err('命令行参数错误：参数之间分隔符错误！');
+      return false;
+    }else{
+      err('命令行参数错误：请输入数字!');
+      return false;
+    }
   }
   return true;
 }
 function judgeSum(){
+  sum=0;
   for(var q = 0;q<arr.length;q++){
-    sum+=arr[q];
+    sum=sum+Number(arr[q]);
   }
-  if(sum<1){err('命令行参数错误：概率空间不封闭！');return false;}
-  else if(sum>1){err('命令行参数错误：概率和超过1！');return false;}
-  return true;
+  //log('sum:',sum);
+  if(sum<0.99999){
+    err('命令行参数错误：概率空间不封闭！');
+    return false;
+  }else if(sum>1){
+    err('命令行参数错误：概率和超过1！');
+    return false;
+  }else{
+    return true;
+  }
+}
+function judgeS(v){
+  arr = v.split(',');
 }
 
 //逐行输入
 function  loopInput(){
   log('请输入信源熵概率分布')
   log('p'+j);
-
+  arr=[];
   stdin.on('data',(data)=>{
+    f2=judgeOne(data);
+    if(f2){
+      arr.push(data.toString('utf8').substring(0,3));
+    }else if(!f2){
+      j--;
+    }
     j++;
     log('p'+j);
-    judgeOne(data);
-    arr.push(data);
   });
-
   stdin.on('end',()=>{
-    for(var m =0;m<arr.length;m++){
-      num = Number(arr[m]);
-      hp += -num*Math.log2(num);
+    if(judgeSum()){  
+      getResult();
+    }else{
+      log('概率空间不封闭，无效的信源熵计算！');
     }
-    log('p=',arr);
-    console.log(hp);
   });
+  return;
 }
+
+//得结果
+function getResult(){
+  for(var m =0;m<arr.length;m++){
+    num = Number(arr[m]);
+    hp += -num*Math.log2(num);
+  }
+  log('p=',arr);
+  console.log('h(p)=',hp.toFixed(3));
+}
+main();
